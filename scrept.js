cfe7c41b7059b6887f86d50560b0a77997831361
Suren <example@gmail.com>
2019-06-13 01:57:45 +0400

    second

---
+++ scrept.js
@@ -0,0 +1,47 @@
+"use strickt";
+
+console.log(4 / 0);
+console.log('string' * 2);
+
+let lol;
+console.log(lol);
+
+let persone = {
+    name: "John",
+    age: 73,
+    isMaried: false
+};
+
+console.log(persone.age);
+
+let arr = ["masiv.png", "terminator.jpg", "googl.bmp"];
+
+console.log(arr[2]);
+
+// alert("Hello World !");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = prompt("Ест ли вам 18?", "Да");
+
+//  console.log(answer);
+
+// console.log(typeof(null));
+
+// console.log(3  + " - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(1565%6);
+console.log("3" === 3);
+
+let isCheked = true,
+    isClose = false;
+
+console.log(isCheked || ! isClose); 
